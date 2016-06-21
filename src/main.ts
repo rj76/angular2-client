dd3355241d5b6096d4cfebe3d06b48cad5b8ddb5
Richard Jansen <richard.jansen@lajos.nl>
2016-06-21 14:19:01 +0200

    added HTTP_PROVIDERS to bootstrap

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { bootstrap } from '@angular/platform-browser-dynamic';
 import { enableProdMode, provide, PLATFORM_DIRECTIVES } from '@angular/core';
 import { LocationStrategy, HashLocationStrategy } from '@angular/common';
+import { HTTP_PROVIDERS } from '@angular/http';
 import { ROUTER_DIRECTIVES } from '@angular/router';
 import { AppComponent, environment, APP_ROUTER_PROVIDERS } from './app/';
 
@@ -10,6 +11,7 @@ if (environment.production) {
 
 bootstrap(AppComponent, [
   APP_ROUTER_PROVIDERS,
+  HTTP_PROVIDERS,
   provide(PLATFORM_DIRECTIVES, {useValue: ROUTER_DIRECTIVES, multi: true}),
   provide(LocationStrategy, {useClass: HashLocationStrategy})
 
